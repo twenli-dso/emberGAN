887deb3639b31ff4986cb52050aadb0666505806
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-21 08:38:20 +0800

    added retrieve_malware code and extract api features

---
+++ extract_api_features.py
@@ -0,0 +1,24 @@
+import os
+import wget
+import csv
+#import ember
+import numpy as np
+import pandas as pd
+
+
+data_dir = "../ember_dataset/"
+
+#emberdf = ember.create_metadata(data_dir)
+emberdf = ember.read_metadata(data_dir)
+#X_train, y_train, X_test, y_test = ember.create_vectorized_features(data_dir)
+X_train, y_train, X_test, y_test = ember.read_vectorized_features(data_dir)
+
+X = np.concatenate((X_train, X_test))
+y = np.concatenate((y_train, y_test))
+df = pd.concat([emberdf, pd.DataFrame(X)], axis=1)
+
+print(df.head)
+
+#mal_sha256 = df[df.label == 1].sha256
+
+
