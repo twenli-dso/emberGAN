b362aa1eb8f6228488d58af6ac97ef4c7d26966e
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-21 10:22:22 +0800

    added extract api features code

--- extract_api_features.py
+++ extract_api_features.py
@@ -1,22 +1,39 @@
-import os
-import ember
-import numpy as np
-import pandas as pd
+import json
+import glob
+from collections import Counter
 
+filepaths = glob.glob('../../ember_dataset/*.jsonl')
+#filepaths = glob.glob('C:/Users/TWenLi/Downloads/*.jsonl')
+for filepath in filepaths:
+    print(filepath)
+    with open (filepath,"r") as infile:
+        for line_num, line in enumerate(infile):
+            apistats_dict = {}
 
-data_dir = "../../ember_dataset/"
+            jsonline = json.loads(line)
 
-#emberdf = ember.create_metadata(data_dir)
-emberdf = ember.read_metadata(data_dir)
-#X_train, y_train, X_test, y_test = ember.create_vectorized_features(data_dir)
-X_train, y_train, X_test, y_test = ember.read_vectorized_features(data_dir)
+            sha256 = jsonline["sha256"]
+            label = jsonline["label"]
+            if label == 0:
+                label = "benign"
+            else:
+                label = "malware"
 
-X = np.concatenate((X_train, X_test))
-y = np.concatenate((y_train, y_test))
-df = pd.concat([emberdf, pd.DataFrame(X)], axis=1)
+            apistats_dict["name"] = sha256
+            apistats_dict["class"] = label
 
-print(df.head)
+            #retrieve api features and count occurrence of each api
+            imports = jsonline["imports"]
+            imports = list(imports.values())
+            imports_flattened = [item for sublist in imports for item in sublist]
+            imports_dict = dict(Counter(imports_flattened))
 
-#mal_sha256 = df[df.label == 1].sha256
+            apistats_dict["apistats"] = imports_dict
 
+            #print(apistats_dict)
 
+            with open('%s.json' %(sha256), 'w') as outfile:
+                json.dump(apistats_dict, outfile)
+
+            #if line_num == 5:
+                #break
