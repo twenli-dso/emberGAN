daac3677eb6837b9f1c79165a160c33cae7c567d
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-21 14:45:26 +0800

    changed output data name

--- generate_input_data.py
+++ generate_input_data.py
@@ -47,4 +47,4 @@ xmal = x[np.where(y==1)]
 ymal = y[np.where(y==1)]
 xben = x[np.where(y==0)]
 yben = y[np.where(y==0)]
-np.savez('./data/data_ember.npz', xmal=xmal, ymal=ymal, xben=xben, yben=yben)
+np.savez('./data/data_ember_2048.npz', xmal=xmal, ymal=ymal, xben=xben, yben=yben)
