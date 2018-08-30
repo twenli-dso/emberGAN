d56dfbf8a6815e141871244a968b10e9c36a3d3c
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-30 15:46:05 +0800

    fixed typo

--- test_ember.py
+++ test_ember.py
@@ -1,10 +1,10 @@
 import ember
 
-modelpath = ../../ember_dataset/model.h5
-filedata = adversarial_ember_samples.json
-raw_feature_paths = [adversarial_ember_samples.json]
-X_path = X_adversarial_test.dat
-y_path = y_adversarial_test.dat
+modelpath = "../../ember_dataset/model.h5"
+filedata = "adversarial_ember_samples.json"
+raw_feature_paths = ["adversarial_ember_samples.json"]
+X_path = "X_adversarial_test.dat"
+y_path = "y_adversarial_test.dat"
 
 ember.vectorize_subset(X_path, y_path, raw_feature_paths, 4096)
 
