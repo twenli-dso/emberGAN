4d1fd93c578dba9be3f1a5cd03e3cde7ddf700bb
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-30 15:42:50 +0800

    added adversarial examples and code to run ember on them

---
+++ test_ember.py
@@ -0,0 +1,11 @@
+import ember
+
+modelpath = ../../ember_dataset/model.h5
+filedata = adversarial_ember_samples.json
+raw_feature_paths = [adversarial_ember_samples.json]
+X_path = X_adversarial_test.dat
+y_path = y_adversarial_test.dat
+
+ember.vectorize_subset(X_path, y_path, raw_feature_paths, 4096)
+
+ember.predict_sample(modelpath, file_data)
