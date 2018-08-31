c089a11565afe6eb22f0e224b4b5a2c0536f3758
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-31 10:14:06 +0800

    vectorize_subset

--- test_ember.py
+++ test_ember.py
@@ -1,11 +1,25 @@
 import ember
+from keras.models import load_model
+import pandas as pd
 
 modelpath = "../../ember_dataset/model.h5"
-filedata = "adversarial_ember_samples.json"
+file_data = "adversarial_ember_samples.json"
 raw_feature_paths = ["adversarial_ember_samples.json"]
 X_path = "X_adversarial_test.dat"
 y_path = "y_adversarial_test.dat"
+#df_path = "../../ember_dataset/df_scaled.pkl"
 
-#ember.vectorize_subset(X_path, y_path, raw_feature_paths, 4096)
+ember.vectorize_subset(X_path, y_path, raw_feature_paths, 4096)
 
-ember.predict_sample(modelpath, file_data)
+'''
+model = load_model(modelpath)
+
+print("Loading scaled features...")
+df = pd.read_pickle(df_path)
+X = df.iloc[:, 4:].values
+print(X)
+y = df.label
+print(y)
+
+y_pred = model.predict(X).reshape(len(y),)
+'''
