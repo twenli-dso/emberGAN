bfd2460488d2485326153a28cef9a9c52605c68a
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-09-04 11:22:56 +0800

    shifted function declaration on top

--- test_ember.py
+++ test_ember.py
@@ -4,6 +4,26 @@ import pandas as pd
 import numpy as np
 from features import PEFeatureExtractor
  
+def separate_by_feature(self, data):
+        """
+        Separate data by feature group to feed multiple input model
+        """
+        extractor = PEFeatureExtractor()
+        dim = 256
+        f_dict = {}
+        end = 0
+
+        for feature in extractor.features:
+            start = end
+            end += feature.dim
+            f_dict[feature.name] = data[..., start:end]
+
+        if data.ndim==1:
+            return [f_dict['histogram'].reshape(1, dim, 1), f_dict['byteentropy'].reshape(1, dim, 1), np.expand_dims(f_dict['strings'], axis=0), np.expand_dims(f_dict['general'], axis=0), np.expand_dims(f_dict['header'], axis=0), np.expand_dims(f_dict['section'], axis=0), np.expand_dims(f_dict['imports'], axis=0), np.expand_dims(f_dict['exports'], axis=0)]
+
+        data_len = len(data)
+        return [f_dict['histogram'].reshape(data_len, dim, 1), f_dict['byteentropy'].reshape(data_len, dim, 1), f_dict['strings'], f_dict['general'], f_dict['header'], f_dict['section'], f_dict['imports'], f_dict['exports']]
+
 
 modelpath = "../../ember_dataset/model.h5"
 #file_data = "adversarial_ember_samples.jsonl"
@@ -27,24 +47,3 @@ y_pred = model.predict(X).reshape(len(y),)
 print("y_pred:",y_pred)
 acc = model.evaluate(X, y)[1]
 print("acc:",acc)
-
-def separate_by_feature(self, data):
-        """
-        Separate data by feature group to feed multiple input model
-        """
-        extractor = PEFeatureExtractor()
-        dim = 256
-        f_dict = {}
-        end = 0
-
-        for feature in extractor.features:
-            start = end
-            end += feature.dim
-            f_dict[feature.name] = data[..., start:end]
-
-        if data.ndim==1:
-            return [f_dict['histogram'].reshape(1, dim, 1), f_dict['byteentropy'].reshape(1, dim, 1), np.expand_dims(f_dict['strings'], axis=0), np.expand_dims(f_dict['general'], axis=0), np.expand_dims(f_dict['header'], axis=0), np.expand_dims(f_dict['section'], axis=0), np.expand_dims(f_dict['imports'], axis=0), np.expand_dims(f_dict['exports'], axis=0)]
-
-        data_len = len(data)
-        return [f_dict['histogram'].reshape(data_len, dim, 1), f_dict['byteentropy'].reshape(data_len, dim, 1), f_dict['strings'], f_dict['general'], f_dict['header'], f_dict['section'], f_dict['imports'], f_dict['exports']]
-
