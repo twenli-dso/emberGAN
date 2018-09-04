176c0517cad52efcc6d821b210aa34f1552e5c55
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-09-04 10:53:26 +0800

    read np array from .dat files

--- test_ember.py
+++ test_ember.py
@@ -1,24 +1,19 @@
 import ember
 from keras.models import load_model
 import pandas as pd
+import numpy as np
 
 modelpath = "../../ember_dataset/model.h5"
 #file_data = "adversarial_ember_samples.jsonl"
 raw_feature_paths = ["adversarial_ember_samples_3.jsonl"]
 X_path = "X_adversarial_test.dat"
 y_path = "y_adversarial_test.dat"
-#df_path = "../../ember_dataset/df_scaled.pkl"
 
-ember.vectorize_subset(X_path, y_path, raw_feature_paths, 369)
+#ember.vectorize_subset(X_path, y_path, raw_feature_paths, 369)
 
-model = load_model(modelpath)
-
-print("Loading scaled features...")
-df = pd.read_pickle(df_path)
-X = df.iloc[:, 4:].values
-print("X.shape:",X.shape)
-y = df.label
-print("y.shape:",y.shape)
+ndim = 128
+X = np.memmap(X_path, dtype=np.float32, mode="r", shape=(369, ndim))
+y = np.memmap(y_path, dtype=np.float32, mode="r", shape=369)
 
 y_pred = model.predict(X).reshape(len(y),)
 print("y_pred:",y_pred)
