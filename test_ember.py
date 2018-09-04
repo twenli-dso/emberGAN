7fdc14c610157c9f92b211012a599a591369ecf7
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-09-04 11:00:52 +0800

    added separate_by_feature

--- test_ember.py
+++ test_ember.py
@@ -2,6 +2,7 @@ import ember
 from keras.models import load_model
 import pandas as pd
 import numpy as np
+from ../ember/embernet import separate_by_feature
 
 modelpath = "../../ember_dataset/model.h5"
 #file_data = "adversarial_ember_samples.jsonl"
@@ -11,9 +12,13 @@ y_path = "y_adversarial_test.dat"
 
 #ember.vectorize_subset(X_path, y_path, raw_feature_paths, 369)
 
+#load X and y from .dat files
 ndim = 128
 X = np.memmap(X_path, dtype=np.float32, mode="r", shape=(369, ndim))
 y = np.memmap(y_path, dtype=np.float32, mode="r", shape=369)
+
+#separate X into 8 feature arrays
+X = separate_by_feature(X)
 
 model = load_model(modelpath)
 
