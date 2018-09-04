4dd3120a9b305b780889dccd512065f3e2389bf2
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-09-04 10:47:25 +0800

    updated test_ember to predict labels

--- test_ember.py
+++ test_ember.py
@@ -4,22 +4,24 @@ import pandas as pd
 
 modelpath = "../../ember_dataset/model.h5"
 #file_data = "adversarial_ember_samples.jsonl"
-raw_feature_paths = ["adversarial_ember_samples_2.jsonl"]
+raw_feature_paths = ["adversarial_ember_samples_3.jsonl"]
 X_path = "X_adversarial_test.dat"
 y_path = "y_adversarial_test.dat"
 #df_path = "../../ember_dataset/df_scaled.pkl"
 
-ember.vectorize_subset(X_path, y_path, raw_feature_paths, 4096)
+ember.vectorize_subset(X_path, y_path, raw_feature_paths, 369)
 
-'''
 model = load_model(modelpath)
 
 print("Loading scaled features...")
 df = pd.read_pickle(df_path)
 X = df.iloc[:, 4:].values
-print(X)
+print("X.shape:",X.shape)
 y = df.label
-print(y)
+print("y.shape:",y.shape)
 
 y_pred = model.predict(X).reshape(len(y),)
-'''
+print("y_pred:",y_pred)
+acc = model.evaluate(X, y)[1]
+print("acc:",acc)
+
