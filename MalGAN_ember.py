af434bd1313efc8ae1e46a0326285a9b36aeb152
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-10-16 16:07:21 +0800

    chnaged to train 50 times after retraining

--- MalGAN_ember.py
+++ MalGAN_ember.py
@@ -38,7 +38,7 @@ class MalGAN():
         self.filename = filename
 
         # Directories and filepaths for blackbox data
-        self.blackbox_num_samples = 8192
+        self.blackbox_num_samples = 2048
         self.jsonl_dir = "./samples_%s/" % (self.blackbox_num_samples)
         self.mal_samples_filepath = "%smalware_samples_%s.jsonl" % (self.jsonl_dir, int(self.blackbox_num_samples * 0.8))
         self.ben_samples_filepath = "%sbenign_samples_%s.jsonl" % (self.jsonl_dir, int(self.blackbox_num_samples * 0.2))
@@ -456,7 +456,7 @@ if __name__ == '__main__':
     malgan = MalGAN()
     malgan.train(epochs=50, batch_size=64)
     malgan.retrain_blackbox_detector()
-    malgan.train(epochs=20, batch_size=64)
+    malgan.train(epochs=50, batch_size=64)
     '''
     for i in range(10):
         malgan.retrain_blackbox_detector()
