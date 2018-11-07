481021df0864070ef0aa6117c627011126d06e4c
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-11-07 11:15:06 +0800

    switch to gpu 0

--- test_ember_function.py
+++ test_ember_function.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.75)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
